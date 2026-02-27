180ca95f363a3d1d62a7a45824e9f7fb4eac1376
Abdulaziz Khader <130017155+aokhader@users.noreply.github.com>
2026-02-26 16:35:24 -0800

    Added minheap structure for dijkstra

--- web-graph/src/algorithms/dijkstra.ts
+++ web-graph/src/algorithms/dijkstra.ts
@@ -1,1 +1,57 @@
 import type { Graph, AlgorithmResult, StepFrame } from './types';
+
+
+
+// MinHeap implementation for Dijkstra's algorithm
+class MinHeap<T> {
+  private data: T[] = [];
+  private cmp: (a: T, b: T) => number;
+
+  constructor(comparator: (a: T, b: T) => number) {
+    this.cmp = comparator;
+  }
+
+  push(item: T): void {
+    this.data.push(item);
+    this._bubbleUp(this.data.length - 1);
+  }
+
+  pop(): T | undefined {
+    if (this.data.length === 0) return undefined;
+    const top = this.data[0];
+    const last = this.data.pop()!;
+    if (this.data.length > 0) {
+      this.data[0] = last;
+      this._siftDown(0);
+    }
+    return top;
+  }
+
+  isEmpty(): boolean { return this.data.length === 0; }
+  size():    number  { return this.data.length; }
+
+  private _bubbleUp(i: number): void {
+    while (i > 0) {
+      const parent = (i - 1) >> 1;
+      if (this.cmp(this.data[i], this.data[parent]) < 0) {
+        [this.data[i], this.data[parent]] = [this.data[parent], this.data[i]];
+        i = parent;
+      } else break;
+    }
+  }
+
+  private _siftDown(i: number): void {
+    const n = this.data.length;
+    while (true) {
+      let smallest = i;
+      const l = 2 * i + 1;
+      const r = 2 * i + 2;
+      if (l < n && this.cmp(this.data[l], this.data[smallest]) < 0) smallest = l;
+      if (r < n && this.cmp(this.data[r], this.data[smallest]) < 0) smallest = r;
+      if (smallest !== i) {
+        [this.data[i], this.data[smallest]] = [this.data[smallest], this.data[i]];
+        i = smallest;
+      } else break;
+    }
+  }
+}
